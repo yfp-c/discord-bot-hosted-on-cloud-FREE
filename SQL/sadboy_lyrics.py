23ee17ec7f1173ceb7ef0a90d6012c3a6c1e2296
Yacob Franco Pereira <98178943+yfp-c@users.noreply.github.com>
2023-12-13 17:26:26 +0000

    Update sadboy_lyrics.py
    updated code to include check for image in column artist_image_url

--- SQL/sadboy_lyrics.py
+++ SQL/sadboy_lyrics.py
@@ -4,27 +4,33 @@ import sqlite3
 conn = sqlite3.connect("sadboy_lyrics.db")
 cursor = conn.cursor()
 
-# Create the table
-# cursor.execute("CREATE TABLE songs (song_id INTEGER PRIMARY KEY, song_name TEXT, artist TEXT, lyrics TEXT)")
-
-# # Prompt the user to enter the song name, artist, and lyrics
-# song_name = input("Enter the song name: ")
-# artist = input("Enter the artist: ")
-# lyrics = input("Enter the lyrics: ")
+# Check if the artist_image_url column exists and add it if not
+cursor.execute("PRAGMA table_info(songs)")
+columns = [column[1] for column in cursor.fetchall()]
+if "artist_image_url" not in columns:
+    cursor.execute("ALTER TABLE songs ADD COLUMN artist_image_url TEXT")
 
 # Prompt the user to enter the lyrics but everything else is filled in
-song_name = "BE MY MELODY"
-artist = "Seo In Guk (서인국)"
+song_name = "On My Ones"
+artist = "MoStack"
 lyrics = input("Enter the lyrics: ")
 
 # Insert a row into the table
-cursor.execute("INSERT INTO songs (song_id, song_name, artist, lyrics) VALUES (NULL, ?, ?, ?)", (song_name, artist, lyrics))
+cursor.execute("INSERT INTO songs (song_name, artist, lyrics) VALUES (?, ?, ?)", (song_name, artist, lyrics))
+song_id = cursor.lastrowid
 
-# # Prompt the user to enter the song_id of the row to delete
-# song_id = input("Enter the song_id of the row to delete: ")
+# Check if there is an existing image URL for the artist
+cursor.execute("SELECT artist_image_url FROM songs WHERE artist=? AND artist_image_url IS NOT NULL AND artist_image_url != '' LIMIT 1", (artist,))
+result = cursor.fetchone()
 
-# # Delete the row from the table
-# cursor.execute("DELETE FROM songs WHERE song_id=?", (song_id,))
+if result:
+    # An image URL exists, update the new record with it
+    artist_image_url = result[0]
+    cursor.execute("UPDATE songs SET artist_image_url=? WHERE song_id=?", (artist_image_url, song_id))
+else:
+    # No image URL exists, prompt for one
+    artist_image_url = input(f"Enter the image URL for the artist {artist}: ")
+    cursor.execute("UPDATE songs SET artist_image_url=? WHERE song_id=?", (artist_image_url, song_id))
 
 # Save the changes to the database
 conn.commit()
